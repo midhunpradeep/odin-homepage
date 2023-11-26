35040b72a56581f1a5fd54b371e782839f1ac2ab
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2023-11-26 13:40:50 +0100

    feat: Animate trailing stars

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const CURSOR_DURATION_MS = 150;
-const STAR_DURATION_MS = CURSOR_DURATION_MS * 3;
-const TIME_BETWEEN_STARS_MS = 100;
+// const STAR_DURATION_MS = CURSOR_DURATION_MS * 3;
+const TIME_BETWEEN_STARS_MS = 200;
 
 function createGlowDot(event, timeout) {
   const dot = document.createElement("div");
@@ -19,7 +19,7 @@ function shouldCreateStar(delay) {
   return currentTime - _lastStarTime >= delay;
 }
 
-function createStar(event, color, timeout) {
+function createStar(event, color) {
   _lastStarTime = new Date().getTime();
   const star = document.createElement("div");
   star.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star-four-points</title><path fill=${color} d="M12,1L9,9L1,12L9,15L12,23L15,15L23,12L15,9L12,1Z" /></svg>`;
@@ -27,7 +27,9 @@ function createStar(event, color, timeout) {
   star.style.left = `${event.pageX}px`;
   star.style.top = `${event.pageY}px`;
   document.body.appendChild(star);
-  setTimeout(() => document.body.removeChild(star), timeout);
+  star.addEventListener("animationend", () => {
+    document.body.removeChild(star);
+  });
 }
 
 function main() {
@@ -50,7 +52,7 @@ function main() {
   window.addEventListener("mousemove", (event) => {
     createGlowDot(event, CURSOR_DURATION_MS);
     if (shouldCreateStar(TIME_BETWEEN_STARS_MS)) {
-      createStar(event, starColors[currentStarColor], STAR_DURATION_MS, 100);
+      createStar(event, starColors[currentStarColor], 100);
       currentStarColor = (currentStarColor + 1) % starColors.length;
     }
   });
