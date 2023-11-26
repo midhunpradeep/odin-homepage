4e31709a975b56a064d519d3f7abbc20454386fe
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2023-11-26 13:24:04 +0100

    feat: Add trailing stars to cursor

--- scripts/index.js
+++ scripts/index.js
@@ -11,9 +11,27 @@ function createGlowDot(event, timeout) {
   setTimeout(() => document.body.removeChild(dot), timeout);
 }
 
+let _lastStarTime = new Date().getTime();
+function createStar(event, color, timeout, delay) {
+  const currentTime = new Date().getTime();
+  if (currentTime - _lastStarTime < delay) {
+    return;
+  }
+
+  _lastStarTime = currentTime;
+  const star = document.createElement("div");
+  star.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star-four-points</title><path fill=${color} d="M12,1L9,9L1,12L9,15L12,23L15,15L23,12L15,9L12,1Z" /></svg>`;
+  star.classList.add("star");
+  star.style.left = `${event.pageX}px`;
+  star.style.top = `${event.pageY}px`;
+  document.body.appendChild(star);
+  setTimeout(() => document.body.removeChild(star), timeout);
+}
+
 function main() {
   window.addEventListener("mousemove", (event) => {
     createGlowDot(event, CURSOR_DURATION_MS);
+    createStar(event, "#FFFFFF", CURSOR_DURATION_MS * 3, 100);
   });
 }
 
